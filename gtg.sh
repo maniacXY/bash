6f7bc22502cda0cecf4188e42680d0c8046aca8b
maniacXY <maniacxy@protonmail.com>
2021-10-24 14:26:21 +0200

    So 24. Okt 14:26:21 CEST 2021-station-push

---
+++ gtg.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# source it in the .bashrc see below
+# <yourterminalcomment>="/home/USER/bashscript.sh
+
+# uncomment the edit, uncomment and go :)
+# dir = one dir above the other folders
+###################################################
+#dir="/home/USER/DIR"
+#function_gtg () {
+#	cd $dir/$workdir
+#}
+
+#case $1 in
+#	COMMAND1)
+#		workdir="REAL_FOLDER_NAME"
+#		function_gtg;;
+#	COMMAND2)	
+#		workdir="FOLDER"
+#		function_gtg;;
+#	COMMANDX)
+#		workdir="FOLDER"
+#		function_gtg;;
+#
+#	*)						#for all other cases
+#		echo "Parameter nicht vorhanden";; 
+#esac
